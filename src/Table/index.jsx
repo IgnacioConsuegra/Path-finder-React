7d2beec1a008a88fcf40ebc552b8aa315b9efbb8
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2024-03-12 20:25:48 +0100

    Now, this works correctly with breath first, well, it does'nt animate as it must, this code just paint our boxed immediately

--- src/Table/index.jsx
+++ src/Table/index.jsx
@@ -5,11 +5,26 @@ import './index.css'
 
 export const ACTIONS = { 
   CREATE_CELLS: 'create-cells',
+  MODIFY_CELL : 'modify-cell'
 }
 function reducer(cells, action) {
-  switch(action.type){
+  switch(action.type)
+  {
     case ACTIONS.CREATE_CELLS : 
-    return action.payload.arr;
+      return action.payload.arr;
+
+    case ACTIONS.MODIFY_CELL:
+      return cells.map(row => {
+        if (row.some(item => item.id === action.payload.id)) {
+          return row.map(item => {
+            if (item.id === action.payload.id) {
+              return { ...item, property: action.payload.value };
+            }
+            return item;
+          });
+        }
+        return row;
+      });
   }
 }
 
@@ -18,27 +33,23 @@ function reducer(cells, action) {
 const Table = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [cells, dispatch] = useReducer(reducer, [[], []])
-
+  const classes = ["empty", "init", "end", "wall", "searching"];
   // eslint-disable-next-line react-hooks/r~ules-of-hooks
   useEffect(() => {
     const table = [];
     for(let i = 0; i < 8; i++){
       const row = [];
       for(let j = 0; j < 8; j++){
-        row.push({row: i, column: j, piece: null});
+        row.push({row: i, column: j});
       }
       table.push(row);
     }
-    dispatch({type: ACTIONS.CREATE_CELLS, payload: {arr: table}})
-    
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  useEffect(() => {
     class Node
     {
-      constructor(id = 0, row = null, column = null, left = null, right = null, top = null, bottom = null, topLeft = null, topRight = null, bottomLeft = null, bottomRight = null)
+      constructor(id = 0, value = 0, row = null, column = null, left = null, right = null, top = null, bottom = null, topLeft = null, topRight = null, bottomLeft = null, bottomRight = null)
       {
         this.id = id;
+        this.value = value;
         this.row = row;
         this.column = column;
         this.right = right;
@@ -53,12 +64,12 @@ const Table = () => {
     }
 
     const myArrNodes = []
-    cells.map((e) => 
+    table.map((e) => 
     {
       myArrNodes.push(
         e.map((element, index) => 
         {
-          return new Node(`${element.row}${element.column}`, element.row, element.column);
+          return new Node(`${element.row}${element.column}`, 0, element.row, element.column);
         })
       )
     })
@@ -68,6 +79,10 @@ const Table = () => {
       constructor(table)
       {
         this.table = table;
+        this.queue = [];
+        this.visited = new Set();
+        this.startNode = null;
+        this.endNode = null;
       }
       bindNodes() 
       {
@@ -89,26 +104,127 @@ const Table = () => {
       {
         console.log(this.table)
       }
+      createInit(x, y)
+      {
+        this.table[x][y]['value'] = 1;
+        this.startNode = this.table[x][y];
+      }
+      createEnd(x, y)
+      {
+        this.table[x][y]['value'] = 2;
+        this.endNode = this.table[x][y];
+      }
+      createWall(x, y)
+      {
+        this.table[x][y]['value'] = 3;
+      }
+      getNode(x, y)
+      {
+        return this.table[x][y];
+      }
+      getNeighbors(node)
+      {
+        const { id, value, row, column, ...rest } = node;
+        const filteredNeighbors = {};
+      
+        for (const key in rest) {
+          if (rest[key] !== null) {
+            filteredNeighbors[key] = rest[key];
+          }
+        }
+      
+        return filteredNeighbors;
+      }
+      moveNodeToLeft(x, y)
+      {
+        const node = this.getNode(x, y);
+        console.log(node);
+        if(node['left'])
+        {
+          node['left'].value = 4;
+        }
+      }
+
+      bfs()
+      {
+        let startNode = this.startNode;
+        let endNode = this.endNode;
+        if(startNode && endNode)
+        {
+          this.queue.push(startNode);
+          this.visited.add(startNode);
+          while(this.queue.length > 0)
+          {
+            let currentNode = this.queue.shift();
+            let neighbors = this.getNeighbors(currentNode);
+            for(let neighbor in neighbors)
+            {
+
+              let myNeighbor = neighbors[neighbor];
+
+              if(myNeighbor === this.endNode)
+              {
+                while(this.queue.length > 0)
+                {
+                  this.queue.pop();
+                }
+                break;
+              }
+              if(myNeighbor["value"] === 3)
+              {
+                continue;
+              }
+              if(!this.visited.has(myNeighbor) && myNeighbor !== null)
+              {
+                this.queue.push(myNeighbor);
+                this.visited.add(myNeighbor);
+                myNeighbor["value"] = 4;
+                // dispatch({type: ACTIONS.MODIFY_CELL,
+                //   payload: {id: myNeighbor["id"], value: 4, column: myNeighbor["column"], row: myNeighbor["row"]}})
+              }
+            }
+          }
+        }
+      }
     }
     const myTable = new Table(myArrNodes);
     myTable.bindNodes();
-    myTable.printAll();
+    myTable.createInit(5, 5);
+    myTable.createEnd(1, 2);
+    // myTable.createWall(5, 3);
+    myTable.createWall(4, 4);
+    myTable.createWall(6, 4);
+    myTable.createWall(7, 4);
+    myTable.createWall(5, 4);
+    myTable.createWall(3, 4);
+    myTable.createWall(2, 4);
+    myTable.createWall(1, 4);
+    myTable.bfs();
+    dispatch({type: ACTIONS.CREATE_CELLS, payload: {arr: myTable}})
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  useEffect(() => {
+  
+    // dispatch({type: ACTIONS.CREATE_CELLS, payload: {arr: myTable}})
+
+    
     
   }, [cells])
 
   return (
     <div className='table'>
       {
-        cells.map((arr, index1) => {
+        cells["table"] && (
+          cells["table"].map((arr, index1) => {
           
-          return arr.map((element, index2) => {
-            if(element.piece){
-              return(<Cell key={index1 + index2} info={element} url={Peon}/>)
-            }
-            return(<Cell key={index1 + index2} info={element}/>)
+            return arr.map((element, index2) => 
+            {
+              
+              return(<Cell key={index1 + index2} myClass={classes[element["value"]]} info={element}/>)
+            })
           })
-        })
+        )
       }
     </div>
   )
--- src/components/Cell/index.jsx
+++ src/components/Cell/index.jsx
@@ -1,20 +1,10 @@
 import './index.css'
-const index = ({url, info, update}) => {
+const index = ({info, myClass}) => {
   const handleClick = () => {
     console.log(info)
   }
   return (
-    <div className='cell' onClick={handleClick}>
-      {
-        url && ( <img src={url} alt="Chess piece" />)
-      }
-      {/* <p>
-        Row:{info.row}
-        <br />
-        Column:{info.column}
-        <br />
-        Piece:{info.piece}
-      </p> */}
+    <div className={`cell ${myClass}`} onClick={handleClick}>
     </div>
   )
 }
